6d1e16525120476c917bb9e7acae231a6c7d0b9d
Niko Escobal <niko.escobal@gmail.com>
2021-10-23 00:31:33 +0800

    Add flex col resizing for comment container on medium screens

--- src/index.js
+++ src/index.js
@@ -22,8 +22,7 @@ const getImages = async () => {
     links: image.links,
     name: image.user.first_name,
     date: image.created_at.split('T')[0],
-  })).filter((image) => image.description !== null && image.description.length < 40 && image.links !== null && image.name !== null);
-
+  })).filter((image) => image.description !== null && image.description.length < 40 && image.links !== null && image.name !== null && image.name);
 
   images.map((image) => {
     const imageUrls = image.urls;
@@ -32,19 +31,24 @@ const getImages = async () => {
     <img class="img-style" src="${imageUrls}"/>
     <figure class="caption-container">
       <figcaption class="caption-content">
-      <svg xmlns="http://www.w3.org/2000/svg" id="${image.id}" class="like h-5 w-5" viewBox="0 0 20 20" fill="red">
-      <path fill-rule="evenodd"
-        d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-        clip-rule="evenodd" />
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
-    </svg>
-        <span class="comment-count">Comments</span>
-      </figcaption>x
+      <div class="comment-container">
+        <svg xmlns="http://www.w3.org/2000/svg" id="${image.id}" class="like h-5 w-5" viewBox="0 0 20 20" fill="red">
+        <path fill-rule="evenodd"
+          d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+          clip-rule="evenodd" />
+        </svg>
+        <h2 class="name">${image.name},
+        ${image.date}</h2>
+      </div> 
+      <div class="comment-container">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+        </svg>
+          <button class="comment-count">Comments</button>
+      </figcaption>
+      </div>
     </figure>
-    <h2 class="name">${image.name},
-      ${image.date}</h2>
+
 
   </article>`);
   });
